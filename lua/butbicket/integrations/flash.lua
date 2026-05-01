12f1ad4c7664e1c4c1413ecb97105703b77063e5
Bosse <bo.nilsson@softhouse.se>
2026-05-01 15:56:53 +0200

    flash colors which make more sense

--- lua/butbicket/integrations/flash.lua
+++ lua/butbicket/integrations/flash.lua
@@ -6,8 +6,8 @@ local M = {}
 function M.highlights()
   return {
     FlashMatch = { fg = colorscheme.hotpink },
-    FlashCurrent = { fg = colorscheme.hotpink, bg = colorscheme.old_mustard },
-    FlashLabel = { fg = colorscheme.hotpink },
+    FlashCurrent = { fg = colorscheme.bright_green, bg = colorscheme.old_mustard },
+    FlashLabel = { fg = colorscheme.bright_green },
   }
 end
 
