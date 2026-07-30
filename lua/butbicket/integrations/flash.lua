705b10213c50b3c0d594df23d34e550febd1e7e8
svampkorg <svampkorg@gmail.com>
2026-07-30 11:12:37 +0200

    fix: FlashLabel
    Let's try something else

--- lua/butbicket/integrations/flash.lua
+++ lua/butbicket/integrations/flash.lua
@@ -77,7 +77,6 @@ function M.highlights()
 
   local lch = oklab.hex_to_oklch(hp)
   local dark = bg_is_dark()
----@diagnostic disable-next-line: unused-local
   local chip, letter = label_chip(lch, dark)
   return {
     FlashMatch = { fg = hp },
@@ -85,7 +84,7 @@ function M.highlights()
       fg = match_fg(lch, dark),
       bg = current_bg(chip),
     },
-    FlashLabel = { fg = match_fg(lch, dark), bg = chip, bold = true },
+    FlashLabel = { fg = chip, bg = letter, bold = true },
   }
 end
 
