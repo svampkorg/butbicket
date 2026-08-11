6fce851c329234b7a23d16a7073fe1b035a09ea5
Bosse <bo.nilsson@softhouse.se>
2026-08-11 13:01:14 +0200

    link NvimTreeSeparator with WinSeparator

--- lua/butbicket/integrations/nvim_tree.lua
+++ lua/butbicket/integrations/nvim_tree.lua
@@ -7,10 +7,7 @@ function M.highlights()
     NvimTreeNormal = { fg = c.mainText, bg = c.sidebarBackground },
     NvimTreeNormalNC = { fg = c.mainText, bg = c.sidebarBackground },
     NvimTreeEndOfBuffer = { fg = c.sidebarBackground, bg = c.sidebarBackground },
-    NvimTreeWinSeparator = {
-      fg = c.sidebarBackground,
-      bg = c.sidebarBackground,
-    },
+    NvimTreeWinSeparator = { link = "WinSeparator" },
     NvimTreeCursorLine = { bg = c.cursorline },
     NvimTreeRootFolder = { fg = c.syntaxKeyword, bold = true },
     NvimTreeFolderName = { fg = c.linkText },
